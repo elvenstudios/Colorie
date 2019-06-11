2b2c97b4d6fadda20f2cd919d56c4682dc6ade2b
Nathan Davis <10ndavis10@gmail.com>
2019-06-11 16:43:32 -0500

    created models

---
+++ lib/models/log_item_model.dart
@@ -0,0 +1,42 @@
+
+
+///
+/// Log Item class
+/// a single log item
+///
+class LogItem {
+
+  final double calories;
+  final double grams;
+
+  LogItem({this.calories, this.grams});
+
+  /*
+   * Calculates calorie density based on calories and grams
+   */
+  double calculateDensity() {
+    return this.calories / this.grams;
+  }
+
+  /*
+   * Calculates a color code based on a given calorie density
+   */
+  String getColorFromDensity() {
+    double density = calculateDensity();
+
+    if (density <= 0.6) {
+      return "green";
+    }
+
+    if (density <= 1.5) {
+      return "yellow";
+    }
+
+    if (density <= 3.8) {
+      return "orange";
+    }
+
+    return "red";
+  }
+
+}
---
+++ lib/models/log_model.dart
@@ -0,0 +1,25 @@
+import './log_item_model.dart';
+
+///
+/// Log class
+/// A list of log items
+/// 
+class Log {
+  List<LogItem> _log = [];
+
+  add(item) {
+    _log.add(item);
+  }
+
+  remove(item) {
+    _log.remove(item);
+  }
+
+  length() {
+    return _log.length;
+  }
+
+  getLog() {
+    return _log;
+  }
+}
