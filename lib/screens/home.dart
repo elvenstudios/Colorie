6ad0aa857f363663c572b482544cf8c78ded4994
Nathan Davis <10ndavis10@gmail.com>
2019-06-12 05:58:06 -0500

    Added nav widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,47 +2,55 @@ import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   Home({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
   _HomeState createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+//  int _counter = 0;
+//
+//  void _incrementCounter() {
+//    setState(() {
+//      _counter++;
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      drawer: Drawer(
+        child: SafeArea(child: Text('Test')),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: SafeArea(
+        child: Container(
+          child: Text('Home'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      bottomNavigationBar: new BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Theme.of(context).primaryColor,
+
+        child:  new Builder(builder: (context) {
+          return new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(icon: Icon(Icons.menu), onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },),
+              IconButton(icon: Icon(Icons.settings), onPressed: () {},),
+            ],
+          );
+        }),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("HI");
+        },
+        child: new Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
