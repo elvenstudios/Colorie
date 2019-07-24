b7a5565c9fd536c93e4cc10f28287693c5bb1039
Justin Dial <justinalandial@gmail.com>
2019-07-24 08:47:26 -0500

    Formatted with dartfmt

--- lib/widgets/circle_painter.dart
+++ lib/widgets/circle_painter.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import "dart:math" show pi;
 import 'package:colorie/models/log_model.dart';
 
-class CirclePainter extends CustomPainter{
-
-
+class CirclePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var paint = Paint();
@@ -22,28 +20,28 @@ class CirclePainter extends CustomPainter{
     //double greenCurve =  logfile.getGreenPercentage()/100*360;
     double greenAngle = pi * 40 / 180; //hardcoded should be line below
     // double greenAngle = pi * greenCurve / 180
-    canvas.drawArc(rect, startAngle, greenAngle,false,paint);
+    canvas.drawArc(rect, startAngle, greenAngle, false, paint);
 
     paint.color = Colors.yellowAccent;
-    startAngle+=greenAngle;
+    startAngle += greenAngle;
     //double yellowCurve = logfile.getYellowPercentage()/100*360;
-    double yellowAngle = pi * 100 / 180;//hardcoded should be line below
+    double yellowAngle = pi * 100 / 180; //hardcoded should be line below
     // double yellowAngle = pi * yellowCurve / 180
-    canvas.drawArc(rect, startAngle, yellowAngle,false,paint);
+    canvas.drawArc(rect, startAngle, yellowAngle, false, paint);
 
     paint.color = Colors.orangeAccent;
-    startAngle+=yellowAngle;
+    startAngle += yellowAngle;
     //double orangeCurve = logfile.getOrangePercentage()/100*360;
-    double orangeAngle = pi * 120 / 180;//hardcoded should be line below
+    double orangeAngle = pi * 120 / 180; //hardcoded should be line below
     // double orangeAngle = pi * orangeCurve / 180
-    canvas.drawArc(rect, startAngle, orangeAngle,false,paint);
+    canvas.drawArc(rect, startAngle, orangeAngle, false, paint);
 
     paint.color = Colors.redAccent;
-    startAngle+=orangeAngle;
+    startAngle += orangeAngle;
     //double redCurve =    logfile.getRedPercentage()/100*360;
-    double redAngle = pi * 100 / 180;//hardcoded should be line below
+    double redAngle = pi * 100 / 180; //hardcoded should be line below
     // double redAngle = pi * redCurve / 180
-    canvas.drawArc(rect, startAngle, redAngle,false,paint);
+    canvas.drawArc(rect, startAngle, redAngle, false, paint);
   }
 
   @override
